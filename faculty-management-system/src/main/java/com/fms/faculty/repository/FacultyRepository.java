84b09cdfb68f39fa8b371457975ec68c35ab22f7
Sandip Patil <96sandypatil@gmail.com>
2023-10-06 11:07:00 +0530

    space added

--- faculty-management-system/src/main/java/com/fms/faculty/repository/FacultyRepository.java
+++ faculty-management-system/src/main/java/com/fms/faculty/repository/FacultyRepository.java
@@ -2,6 +2,8 @@ package com.fms.faculty.repository;
 
 import java.util.List;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
